8975677889674c3b00e02aa4b8554d67afb7191a
Dragon-Git <1762578117@qq.com>
2025-03-24 21:28:29 +0800

    install verilator use cmake

--- script/install_verilator.sh
+++ script/install_verilator.sh
@@ -9,18 +9,17 @@ if [ ! -d /home/runner/work/verilator ]; then
 fi
 
 cd /home/runner/work/verilator
+cd build
 
-# Returns non-zero if no .o files exist
-find -name '*.o' | grep -q .
+# Debug
+cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CXX_STANDARD=17 \
+-DCFG_CXXFLAGS_STD="-std=gnu++17" \
+-DCFG_CXXFLAGS_STD_NEWEST="-std=gnu++17" \
+-DCFG_CXXFLAGS_COROUTINES="-std=gnu++20" \
+-DCFG_CXXFLAGS_PCH_I="-include-pch" \
+-DCFG_CXXFLAGS_NO_UNUSED="-faligned-new -fbracket-depth=4096 -fcf-protection=none -Qunused-arguments -Wno-bool-operation -Wno-c++11-narrowing -Wno-constant-logical-operand -Wno-non-pod-varargs -Wno-parentheses-equality -Wno-shadow -Wno-sign-compare -Wno-tautological-bitwise-compare -Wno-tautological-compare -Wno-uninitialized -Wno-unused-but-set-parameter -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable" \
+-DCFG_CXXFLAGS_WEXTRA="-Wextra -Wfloat-conversion -Wthread-safety" \
 
-# If .o files don't exist, do autoconf and do make
-if [ $? -ne 0 ]; then
-    # git checkout v5.028
-    autoconf
-    ./configure
-    make -j `nproc`
-else
-    echo "Verilator already built, skipping compile/install steps"
-fi
-make install
-
+cmake --build . --config Release
+cmake --install .
