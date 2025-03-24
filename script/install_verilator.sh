10e52ae7f711c11a1cdd489fd9bcbed10fa998bb
Dragon-Git <1762578117@qq.com>
2025-03-24 21:53:15 +0800

    Fixed ci

--- script/install_verilator.sh
+++ script/install_verilator.sh
@@ -17,7 +17,6 @@ cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
 -DCFG_CXXFLAGS_STD="-std=gnu++17" \
 -DCFG_CXXFLAGS_STD_NEWEST="-std=gnu++17" \
 -DCFG_CXXFLAGS_COROUTINES="-std=gnu++20" \
--DCFG_CXXFLAGS_PCH_I="-include-pch" \
 -DCFG_CXXFLAGS_NO_UNUSED="-faligned-new -fcf-protection=none -Wno-bool-operation -Wno-c++11-narrowing -Wno-constant-logical-operand -Wno-non-pod-varargs -Wno-parentheses-equality -Wno-shadow -Wno-sign-compare -Wno-tautological-bitwise-compare -Wno-tautological-compare -Wno-uninitialized -Wno-unused-but-set-parameter -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable" \
 -DCFG_CXXFLAGS_WEXTRA="-Wextra -Wfloat-conversion -Wthread-safety" \
 
