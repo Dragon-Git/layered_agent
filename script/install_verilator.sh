e0d2dbf5bb6852ae3f749faf0d982fd54497e46a
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2025-03-25 08:48:13 +0800

    更新 install_verilator.sh

--- script/install_verilator.sh
+++ script/install_verilator.sh
@@ -13,11 +13,11 @@ cd /home/runner/work/verilator/build
 
 # Debug
 cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_CXX_COMPILER="g++" \
 -DCMAKE_CXX_STANDARD=17 \
 -DCFG_CXXFLAGS_STD="-std=gnu++17" \
 -DCFG_CXXFLAGS_STD_NEWEST="-std=gnu++17" \
 -DCFG_CXXFLAGS_COROUTINES="-std=gnu++20" \
+-DCFG_CXXFLAGS_PCH_I="-include" \
 -DCFG_CXXFLAGS_NO_UNUSED="-faligned-new -fcf-protection=none -Wno-bool-operation -Wno-subobject-linkage -Wno-constant-logical-operand -Wno-non-pod-varargs -Wno-parentheses-equality -Wno-shadow -Wno-sign-compare -Wno-tautological-bitwise-compare -Wno-tautological-compare -Wno-uninitialized -Wno-unused-but-set-parameter -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable" \
 -DCFG_CXXFLAGS_WEXTRA="-Wextra -Wfloat-conversion -Wthread-safety" \
 
